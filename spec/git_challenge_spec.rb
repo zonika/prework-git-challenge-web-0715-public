d1a7753e3dc942104b9ced8f888cef29a1891669
Katie Hoffman <ktahoffman@gmail.com>
2015-01-05 12:37:47 -0500

    make answers more flexible, ground commands in real life

--- spec/git_challenge_spec.rb
+++ spec/git_challenge_spec.rb
@@ -1,51 +1,52 @@
 describe "Git Challenge" do
+
   it 'initializes an empty git repository' do
-    your_answer = __ # replace the __ with your answers in "quotes"
-    expect(your_answer).to eq(answer_1)
+    your_answer = __
+    expect(your_answer).to satisfy {|s| answer_1.include?(s.downcase) }
   end
 
   it 'stages all files to be committed' do
     your_answer = __
-    expect(your_answer).to eq(answer_2) 
+    expect(your_answer).to satisfy {|s| answer_2.include?(s.downcase) }
   end
 
   it 'commits staged files with the message "initial commit"' do 
     your_answer = __
-    expect(your_answer).to eq(answer_3)
+    expect(your_answer).to satisfy {|s| answer_3.include?(s.downcase) }
   end
 
-  it 'adds a link called origin to a remote repository to the local repository' do 
+  it "adds a remote, called origin, which points to a repo located at https://github.com/flatiron/remote.git" do 
     your_answer = __
-    expect(your_answer).to eq(answer_4)
+    expect(your_answer).to satisfy {|s| answer_4.include?(s.downcase) }
   end
 
-  it 'pushes the committed changes to that newly added remote repository' do 
+  it "pushes the committed changes on the master branch to a remote repo which the 'origin' remote points to" do 
     your_answer = __
-    expect(your_answer).to eq(answer_5) 
+    expect(your_answer).to satisfy {|s| answer_5.include?(s.downcase) }
   end
 
   it 'creates a new branch called new-feature' do 
     your_answer = __
-    expect(your_answer).to eq(answer_6)
+    expect(your_answer).to satisfy {|s| answer_6.include?(s.downcase) }
   end
 
   it 'switches to the new-feature branch' do 
     your_answer = __
-    expect(your_answer).to eq(answer_7)
+    expect(your_answer).to satisfy {|s| answer_7.include?(s.downcase) }
   end
 
   it 'brings down changes from the remote repository master branch to the local repository' do 
     your_answer = __
-    expect(your_answer).to eq(answer_8) 
+    expect(your_answer).to satisfy {|s| answer_8.include?(s.downcase) }
   end
 
-  it 'is the actions of copying an existing repository onto your github account' do 
+  it 'is the actions of copying an existing repository onto your GitHub account' do 
     your_answer = __
-    expect(your_answer).to eq(answer_9) 
+    expect(your_answer).to satisfy {|s| answer_9.include?(s.downcase) }
   end
 
-  it 'gets a copy of an existing repository locally onto your computer' do 
+  it 'makes a local copy of an existing remote repository which is located at "https://github.com/flatiron/remote.git"' do 
     your_answer = __
-    expect(your_answer).to eq(answer_10) 
+    expect(your_answer).to satisfy {|s| answer_10.include?(s.downcase) }
   end  
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,20 @@
-require 'base64'
+require "base64"
+require "json"
 
-RSpec.configure do |config| 
-  encoded_answers = "Z2l0IGluaXQsIGdpdCBhZGQgLiwgZ2l0IGNvbW1pdCAtbSAnaW5pdGlhbCBj\nb21taXQnLCBnaXQgcmVtb3RlIGFkZCBvcmlnaW4sIGdpdCBwdXNoIG9yaWdp\nbiBtYXN0ZXIsIGdpdCBicmFuY2ggbmV3LWZlYXR1cmUsIGdpdCBjaGVja291\ndCBuZXctZmVhdHVyZSwgZ2l0IHB1bGwgb3JpZ2luIG1hc3RlciwgZm9ya2lu\nZywgZ2l0IGNsb25l\n"
-  decoded_answers = Base64.decode64(encoded_answers)
+RSpec.configure do |config|
 
-  answers = decoded_answers.split(", ")
+  encoded_answers = "eyIxIjpbImdpdCBpbml0Il0sIjIiOlsiZ2l0IGFkZCAuIiwiZ2l0IGFkZCAt\nLWFsbCIsImdpdCBhZGQgLUEiXSwiMyI6WyJnaXQgY29tbWl0IC1tICdpbml0\naWFsIGNvbW1pdCciLCJnaXQgY29tbWl0IC1tIFwiaW5pdGlhbCBjb21taXRc\nIiJdLCI0IjpbImdpdCByZW1vdGUgYWRkIG9yaWdpbiBodHRwczovL2dpdGh1\nYi5jb20vZmxhdGlyb24vcmVtb3RlLmdpdCJdLCI1IjpbImdpdCBwdXNoIG9y\naWdpbiBtYXN0ZXIiXSwiNiI6WyJnaXQgYnJhbmNoIG5ldy1mZWF0dXJlIl0s\nIjciOlsiZ2l0IGNoZWNrb3V0IG5ldy1mZWF0dXJlIl0sIjgiOlsiZ2l0IHB1\nbGwgb3JpZ2luIG1hc3RlciJdLCI5IjpbImZvcmtpbmciLCJmb3JrIl0sIjEw\nIjpbImdpdCBjbG9uZSBodHRwczovL2dpdGh1Yi5jb20vZmxhdGlyb24vcmVt\nb3RlLmdpdCJdfQ==\n"
+  decoded_json = Base64.decode64(encoded_answers)
+  answers = JSON.parse(decoded_json)
 
-  answers.each_with_index do |answer, index|
-    define_method("answer_#{index+1}") do 
-      answer
+  answers.each do |index, answers|
+    define_method("answer_#{index}") do 
+      answers
     end
   end
+
+end
+
+def __
+  raise 'Replace the underscore with your answer in "quotes"'
 end
