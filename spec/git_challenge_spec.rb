d58f053a4c5239e094e046dfb708a403b7f41649
Katie Hoffman <ktahoffman@gmail.com>
2015-01-06 10:28:58 -0500

    update tests so that only #2,3,9 have multiple right answers

--- spec/git_challenge_spec.rb
+++ spec/git_challenge_spec.rb
@@ -2,7 +2,7 @@ describe "Git Challenge" do
 
   it 'initializes an empty git repository' do
     your_answer = __
-    expect(your_answer).to satisfy {|s| answer_1.include?(s.downcase) }
+    expect(your_answer).to eq(answer_1) 
   end
 
   it 'stages all files to be committed' do
@@ -17,27 +17,27 @@ describe "Git Challenge" do
 
   it "adds a remote, called origin, which points to a repo located at https://github.com/flatiron/remote.git" do 
     your_answer = __
-    expect(your_answer).to satisfy {|s| answer_4.include?(s.downcase) }
+    expect(your_answer).to eq(answer_4)
   end
 
   it "pushes the committed changes on the master branch to a remote repo which the 'origin' remote points to" do 
     your_answer = __
-    expect(your_answer).to satisfy {|s| answer_5.include?(s.downcase) }
+    expect(your_answer).to eq(answer_5) 
   end
 
   it 'creates a new branch called new-feature' do 
     your_answer = __
-    expect(your_answer).to satisfy {|s| answer_6.include?(s.downcase) }
+    expect(your_answer).to eq(answer_6)
   end
 
   it 'switches to the new-feature branch' do 
     your_answer = __
-    expect(your_answer).to satisfy {|s| answer_7.include?(s.downcase) }
+    expect(your_answer).to eq(answer_7) 
   end
 
   it 'brings down changes from the remote repository master branch to the local repository' do 
     your_answer = __
-    expect(your_answer).to satisfy {|s| answer_8.include?(s.downcase) }
+    expect(your_answer).to eq(answer_8)
   end
 
   it 'is the actions of copying an existing repository onto your GitHub account' do 
@@ -47,6 +47,6 @@ describe "Git Challenge" do
 
   it 'makes a local copy of an existing remote repository which is located at "https://github.com/flatiron/remote.git"' do 
     your_answer = __
-    expect(your_answer).to satisfy {|s| answer_10.include?(s.downcase) }
+    expect(your_answer).to eq(answer_10) 
   end  
 end
